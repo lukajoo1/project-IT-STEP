38ce3ad1022cee9e2947606ae772ed71d801cfd5
lukajoo1 <lukajokhadze97@gmail.com>
2025-10-26 14:57:12 -0700

    add project to github

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const burgerIcon = document.querySelector(".burger"); // Changed from .burger-btn to .burger
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerClose = document.querySelector(".burger-close");
+const burgerList = document.querySelectorAll(".burger-menu-nav a");
+
+console.log(burgerList);
+
+function dissapear() {
+  if (burgerMenu.classList.contains("active")) {
+    burgerIcon.style.display = "none";
+    burgerClose.style.display = "block";
+  } else {
+    burgerIcon.style.display = "block";
+    burgerClose.style.display = "none";
+  }
+}
+
+burgerIcon.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  document.body.classList.remove("no-scroll");
+  dissapear();
+});
+
+burgerClose.addEventListener("click", () => {
+  burgerMenu.classList.remove("active");
+  dissapear();
+});
+
+burgerList.forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.remove("active");
+    dissapear();
+  });
+});
